e2ba255cf54e91dc5ec094f6566db2d983a29f27
Dinuka Rathnayaka <112457556+Dinuka-Rathnayake@users.noreply.github.com>
2023-06-30 20:53:40 +0530

    group succussfully

---
+++ findBestStudentForSubject.py
@@ -0,0 +1,29 @@
+lines = None
+
+#import file
+with open("F:\\Programing\\python\\first\mini-project\\data.txt") as file: 
+    lines = file.readlines()
+
+#check import
+if not lines:
+    print("No lines imported !!! ")
+
+#get a needed lines         
+marks_lines = lines[1:]
+
+#group
+subject_marks = {}
+for line in marks_lines:
+    entries = line.split(",")
+    name = entries[0].strip()
+    subject = entries[1].strip()
+    marks = int(entries[2].strip())
+    
+    if subject not in subject_marks:
+        subject_marks[subject] = {}
+
+    subject_marks[subject][name] = marks
+
+print(subject_marks)
+
+
